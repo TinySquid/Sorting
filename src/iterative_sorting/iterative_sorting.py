835d44852d91de4a0851fa94c677b8b8698c131d
Michael Nunes <tinysquid.git@gmail.com>
2020-03-16 12:43:20 -0700

    Add selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,32 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+import random
+
+
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        smallest_index = i
 
+        # Find smallest element
+        for cur_index in range(smallest_index + 1, len(arr)):
+            if arr[cur_index] < arr[smallest_index]:
+                smallest_index = cur_index
 
-
-        # TO-DO: swap
-
-
-
+        # Swap
+        arr.insert(i, arr.pop(smallest_index))
 
     return arr
 
 
+# Test - Should print list with 0 - 9 ascending
+print(selection_sort(random.sample(range(10), 10)))
+
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
